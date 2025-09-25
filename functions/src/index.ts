91d4df087493c6924b038da9edb8833e0d96d17b
gltknky <gltknky@gmail.com>
2025-09-25 20:10:45 +0300

    .

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,14 @@ import { setGlobalOptions } from 'firebase-functions/v2';
 // Set global options for all functions BEFORE importing any functions
 setGlobalOptions({ region: 'europe-west1', maxInstances: 10 });
 
+import { onCall } from 'firebase-functions/v2/https';
+import userService from './services/user.service';
 import { onBusinessVerificationStatusChange, onUserProfileUpdate } from "./triggers";
+
+
+export const setAdminUserClaims = onCall(async (_) => {
+  return userService.setAdminsClaims();
+});
 
 // Export the business verification status change trigger
 export { onBusinessVerificationStatusChange, onUserProfileUpdate };
